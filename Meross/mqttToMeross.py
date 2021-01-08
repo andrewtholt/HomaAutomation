1aaaa0d0984352ff6d950e95d681d3434753bc17
Andrew Holt <andrewtholt60@gmail.com>
2021-01-08 08:05:18 +0000

    Subscribe to specific topics, started to add in Meroos cloud integration

--- Meross/mqttToMeross.py
+++ Meross/mqttToMeross.py
@@ -11,6 +11,9 @@ import sys
 import getopt
 import json
 
+from meross_iot.http_api import MerossHttpClient
+from meross_iot.manager import MerossManager
+
 
 config = {}
 verbose = False
@@ -40,7 +43,11 @@ async def simple_example(queue):
     count = 0
     async with Client("192.168.10.124") as client:
         async with client.filtered_messages("/home/house/#") as messages:
-            await client.subscribe("/home/house/#")
+            for n in config['meross']:
+                print("simple_example",config['meross'][n]['mqtt_sub'])
+                await client.subscribe(config['meross'][n]['mqtt_sub'])
+
+#            await client.subscribe("/home/house/#")
             async for message in messages:
                 print(message.topic)
                 print(message.payload.decode())
@@ -67,8 +74,20 @@ async def consumer(q):
     while True:
         data = await q.get()
         print("Here :",data)
+        topic=data.split(":")[0]
+        msg  =data.split(":")[1]
+        path = topic.split("/") 
+        path.pop(0)
+
+        name = path[2]
+
+        print("\tTopic:" + topic)
+        print("\tMsg  :" + msg)
+        print("\tName :" + name)
+
 
 def main():
+    global config
 
     try:    
         opts, args = getopt.getopt( sys.argv[1:], "c:hvn:", ["config=", "help","name="])    
@@ -100,9 +119,28 @@ def main():
 
     asyncio.ensure_future(mqttMain(queue))
     asyncio.ensure_future(consumer(queue))
+    asyncio.ensure_future(merossMain(queue))
 
     loop.run_forever()
 
 # asyncio.run(main())
+async def merossMain(q):
+    global plugs
+    print("Meross Main")
+    # Setup the HTTP client API from user-password
+    http_api_client = await MerossHttpClient.async_from_user_password(email=EMAIL, password=PASSWORD)
+
+    print("HERE")
+    # Setup and start the device manager
+    manager = MerossManager(http_client=http_api_client)
+    await manager.async_init()
+
+    # Retrieve all the MSS210 devices that are registered on this account
+    await manager.async_device_discovery()
+    plugs = manager.find_devices(device_type="mss210")
+
+    for b in plugs:
+        print(b)
+
 main()
 
